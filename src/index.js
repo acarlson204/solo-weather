7bd78473827e4966c91b5e648ae0ca814d19ec93
Abby Carlson <abbylcarlson@gmail.com>
2021-07-18 10:33:18 -0500

    added map to the right, added daily forecast

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-pk.eyJ1IjoiYWNhcmxzb24yMDQiLCJhIjoiY2tyOGl1Nm9iMmYzcDJudGNjdnJ6emhscSJ9.JDYEZfT1zCzOyFaqUIl43g
+displayTemp(response) {
+    let tempElement = document.querySelector("#temp")
+    tempElement.innerHTML = response.data.main.temp;
+}
+
+
+let api = `https://api.openweathermap.org/data/2.5/weather?q=winnipeg&appid=03ea91762285df0d5fb999b760075dea&units=metric`
+axios.get(api).then(displayTemp);
